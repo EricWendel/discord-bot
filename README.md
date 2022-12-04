e7aa750fcaceccdac52a8aa30565fc324c5bf4a4
EricWendel <eawendel19@gmail.com>
2022-12-03 20:34:30 -0600

    Setup slash commands

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # discord-bot
+
+Learning to make a discord bot
+
+### .env requirements
+
+- TOKEN = the token for the discord bot
+
+### Helpful links
+
+- [Starter tutorial](https://sabe.io/tutorials/how-to-build-discord-bot-typescript)
---
+++ src/Bot.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+import { Client, ClientOptions } from "discord.js";
+import interactionCreate from "./listeners/interactionCreate";
+import ready from "./listeners/ready";
+
+console.log("Bot is starting...");
+
+const client = new Client({
+  intents: [],
+});
+
+ready(client);
+interactionCreate(client);
+
+client.login(process.env.TOKEN);
+
+//console.log(client);
